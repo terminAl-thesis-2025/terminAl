ec8256a7b0c17adfecd7ddb6429daa4f17b55b7c
m <thesis_fhgr@proton.me>
2025-05-20 15:05:11 +0200

    Improved Info display

--- functions/terminal_guard.py
+++ functions/terminal_guard.py
@@ -1,5 +1,6 @@
 # Standard Bibliotheken
 import asyncio
+import json
 import os
 
 # Externe Bibliotheken
@@ -9,6 +10,8 @@ from dotenv import load_dotenv
 from huggingface_hub import login
 # Umgebungsvariablen aus .env Datei laden
 load_dotenv()
+# Hole den Pfad zur Terminal-Anwendung aus den Umgebungsvariablen
+terminal_path = os.getenv("TERMINAL_PATH")
 
 class TerminAlGuard:
     """
@@ -17,20 +20,20 @@ class TerminAlGuard:
     returned by Ollama, and prints a warning if they differ.
     """
 
-    def __init__(
-        self,
-        model_name: str = "terminAl-thesis-2025/deberta-v3-base-terminAl-guard"
-    ):
-
+    def __init__(self):
         # load a sync text-classification pipeline
         token = os.getenv("HF_TOKEN")
         if token:
             login(token)
 
+        self.settings = json.load(
+            open(terminal_path + "settings/settings.json"))  # Lädt Einstellungen aus der JSON-Datei
+        self.guard_settings = self.settings.get("guard_settings", None)
+
         self.classifier = pipeline(
             "text-classification",
-            model=model_name,
-            tokenizer=model_name,
+            model=self.guard_settings.get("guard_model", "terminAl-thesis-2025/deberta-v3-base-terminAl-guard"),
+            tokenizer=self.guard_settings.get("guard_model", "terminAl-thesis-2025/deberta-v3-base-terminAl-guard"),
             truncation=True,
             max_length=512,
             return_all_scores=False,
